8d2fe34d3af46d32b8356b3c6c5b693563f8b9b2
TCab <tihare.cab@gmail.com>
2026-05-20 11:40:16 -0400

    err:DB connection

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"log"
-
+    "os"
 	"Validador-de-Voto-de-Elecciones/handler"
 	"Validador-de-Voto-de-Elecciones/repository"
 	"Validador-de-Voto-de-Elecciones/router"
@@ -10,11 +10,18 @@ import (
 )
 
 func main() {
-	repo := repository.NewVotanteRepository()
-	svc := service.NewVotanteService(repo)
-	h := handler.NewVotanteHandler(svc)
-	r := router.NewRouter(h)
-
-	log.Println("Servidor iniciado en :8080")
-	log.Fatal(r.Run(":8080"))
+    host := os.Getenv("DB_HOST")
+    user := os.Getenv("DB_USER")
+    password := os.Getenv("DB_PASSWORD")
+    dbname := os.Getenv("DB_NAME")
+    connStr := "host=" + host + " user=" + user + " password=" + password + " dbname=" + dbname + " sslmode=disable"
+    repo, err := repository.NewPostgresVotanteRepository(connStr)
+    if err != nil {
+        log.Fatalf("Error conectando a DB: %v", err)
+    }
+    svc := service.NewVotanteService(repo)
+    h := handler.NewVotanteHandler(svc)
+    r := router.NewRouter(h)
+    log.Println("Servidor iniciado en :8080")
+    log.Fatal(r.Run(":8080"))
 }
